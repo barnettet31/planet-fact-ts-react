26efbc6984685e8be7518f45aa445249022cf006
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-09-07 20:19:42 -0400

    added alt styles to body text component

--- src/components/text/body.component.tsx
+++ src/components/text/body.component.tsx
@@ -1,10 +1,10 @@
 import { text } from "stream/consumers"
 
 type IBodyProps = {
-
+    additionalStyles?:string;
     children:JSX.Element | string;
     }
-export function BodyText ({children}:IBodyProps){
-    return <p className='font-body text-[14px] leading-[25px]'>{children}</p>
+export function BodyText ({additionalStyles, children}:IBodyProps){
+    return <p className={`font-body text-[14px] leading-[25px] ${additionalStyles}`}>{children}</p>
 
 }
