34087b02ba7b07449679e9d2f0f00b94f1f0892a
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-09-07 20:52:05 -0400

    finished selecting list for mobile view

--- src/routes/planet.route.tsx
+++ src/routes/planet.route.tsx
@@ -1,11 +1,111 @@
-import React from 'react'
-import { IPlanet } from '../types/planet.type'
+import React, { useState } from "react";
+import { BodyText } from "../components/text/body.component";
+import { IPlanet } from "../types/planet.type";
 
-type PlanetProps ={
-    planet:IPlanet
+type PlanetProps = {
+  planet: IPlanet;
+};
+interface ISelectedListProps {
+  selectedItem: string;
+  planet: string;
+  updateSelected: (contentToUpdateTo: string) => void;
 }
+function SelectedList({ selectedItem, updateSelected, planet }: ISelectedListProps) {
+  const planets = [
+    {
+      name: "Mercury",
+      color: "after:bg-[#DEF4FC]",
+    },
+    {
+      name: "Venus",
+      color: "after:bg-[#F7CC7F]",
+    },
+    {
+      name: "Earth",
+      color: "after:bg-[#545BFE]",
+    },
+    {
+      name: "Mars",
+      color: "after:bg-[#FF6A45]",
+    },
+    {
+      name: "Jupiter",
+      color: "after:bg-[#ECAD7A]",
+    },
+    {
+      name: "Saturn",
+      color: "after:bg-[#FCCB6B]",
+    },
+    {
+      name: "Uranus",
+      color: "after:bg-[#65F0D5]",
+    },
+    {
+      name: "Neptune",
+      color: "after:bg-[#497EFA]",
+    },
+  ];
 
-
-export function  PlanetPage({planet}:PlanetProps){
-    return <h1 className="text-3xl font-bold">{planet.name}</h1>
+  return (
+    <ul className="flex justify-between w-full py-5 border-b-white/[0.2] border-b px-6">
+      <li
+        onClick={() => updateSelected("overview")}
+        className={`relative after:absolute ${
+          selectedItem === "overview" ? "after:scale-x-100" : "after:scale-x-0"
+        } after:ease-in-out after:origin-left after:duration-200 after:content-[''] after:h-[4px] after:w-full  after:-bottom-[80%] ${planets.find(element=>element.name === planet)?.color}`}
+      >
+        <BodyText
+          additionalStyles={
+            selectedItem === "overview" ? "opacity-100" : "opacity-50"
+          }
+        >
+          Overview
+        </BodyText>
+      </li>
+      <li
+        onClick={() => updateSelected("structure")}
+        className={`relative after:absolute ${
+          selectedItem === "structure" ? "after:scale-x-100" : "after:scale-x-0"
+        }  after:content-[''] after:h-[4px] after:duration-200 after:ease-in-out after:origin-left after:w-full after:-bottom-[80%]  ${planets.find(element=>element.name === planet)?.color}`}
+      >
+        <BodyText
+          additionalStyles={
+            selectedItem === "structure" ? "opacity-100" : "opacity-50"
+          }
+        >
+          Structure
+        </BodyText>
+      </li>
+      <li
+        onClick={() => updateSelected("surface")}
+        className={`relative after:absolute ${
+          selectedItem === "surface" ? "after:scale-x-100" : "after:scale-x-0"
+        } after:content-[''] after:h-[4px]  after:duration-200 after:ease-in-out after:origin-left after:w-full after:-bottom-[80%]  ${planets.find(element=>element.name === planet)?.color}`}
+      >
+        <BodyText
+          additionalStyles={
+            selectedItem === "surface" ? "opacity-100" : "opacity-50"
+          }
+        >
+          Surface
+        </BodyText>
+      </li>
+    </ul>
+  );
+}
+export function PlanetPage({ planet }: PlanetProps) {
+  const [selectedItem, setSelectedItem] = useState("overview");
+  const handleSetSelected = (contentToUpdateTo: string) =>
+    setSelectedItem(contentToUpdateTo);
+  return (
+    <div className="">
+      <div className="md:hidden">
+        <SelectedList
+          planet={planet.name}
+          updateSelected={handleSetSelected}
+          selectedItem={selectedItem}
+        />
+      </div>
+    </div>
+  );
 }
