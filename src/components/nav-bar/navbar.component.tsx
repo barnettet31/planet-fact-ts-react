19742106c9c1d2cd6db8a2e5af930bd0baa9ad37
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-09-07 20:19:26 -0400

    rectified navbar issue on media query

--- src/components/nav-bar/navbar.component.tsx
+++ src/components/nav-bar/navbar.component.tsx
@@ -50,7 +50,7 @@ interface IPlanetPathData  {
     linkClickedFunction: () => void;
 }
 function PlanetLink ({name, path, color, linkClickedFunction}:IPlanetPathData){
-    return ( <Link onClick={linkClickedFunction} className="w-full" to={path}><li className="w-full "><div className="flex items-center justify-between w-full py-5 md:pb-7 md:py-0 lg:pb-0"><div className="flex items-center gap-6"><div className={`w-5 h-5 rounded-full ${color} md:hidden`}></div><p className="font-body font-bold font-[15px] leading-[25px] tracking-[1.36px]">{name.toUpperCase()}</p></div><img className="w-1 h-2 md:hidden" alt='' src={`${process.env.PUBLIC_URL}/images/icon-chevron.svg`}/></div></li></Link>)
+    return ( <Link onClick={linkClickedFunction} className="w-full cursor-pointer" to={path}><li className="w-full "><div className="flex items-center justify-between w-full py-5 md:pb-7 md:py-0 lg:pb-0"><div className="flex items-center gap-6"><div className={`w-5 h-5 rounded-full ${color} md:hidden`}></div><p className="font-body font-bold font-[15px] leading-[25px] tracking-[1.36px] lg:opacity-75 lg:hover:opacity-100">{name.toUpperCase()}</p></div><img className="w-1 h-2 md:hidden" alt='' src={`${process.env.PUBLIC_URL}/images/icon-chevron.svg`}/></div></li></Link>)
 }
 export function NavBar (){
     const [navShown, setNavShown ] = useState(false);
@@ -62,7 +62,7 @@ export function NavBar (){
             <Link to='/'><h1 className="font-display uppercase text-[28px] leading-9 -tracking-[1.05px] ">The Planets</h1></Link>
             <img alt="" onClick={handleNavBurgerClick} className="w-6 h-4 md:hidden" src={`${process.env.PUBLIC_URL}/images/icon-hamburger.svg`}/>
             </div>
-            <nav className={`fixed  h-screen w-screen  bg-[#070724] md:bg-transparent md:static md:w-full md:h-auto ease-in-out duration-200 top-[69px]  px-6 py-11 md:py-2 left-0 origin-top ${navShown? 'scale-y-100 opacity-100 md:opacity-100 md:scale-y-100':'scale-y-0 opacity-70 '} `}>
+            <nav className={`fixed  h-screen w-screen  bg-[#070724] md:bg-transparent md:static md:w-full md:h-auto ease-in-out duration-200 top-[69px]  px-6 py-11 md:py-2 left-0 origin-top ${navShown? 'scale-y-100 opacity-100 md:opacity-100 md:scale-y-100':'scale-y-0 opacity-70 md:scale-y-100 md:opacity-100 '} `}>
                 <ul className='flex flex-col items-center justify-center md:flex-row md:justify-evenly'>
                     {planets.map((planet)=><PlanetLink key={planet.name} linkClickedFunction={handleNavBurgerClick} {...planet}/>)}
                     
