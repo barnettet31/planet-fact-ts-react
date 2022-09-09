8aac50f0d88d5ebf203489d17b86808eed5bcf2f
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-09-08 20:50:56 -0400

    removed colors from button for use in other components

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,3 +1,4 @@
+import { activeColors } from "../../utils/helpers";
 import { Heading } from "../text/heading.component";
 
 type IButtonProps = {
@@ -5,18 +6,7 @@ type IButtonProps = {
     planet:string;
     
 }
-const activeColors:{
-    [key:string]:string
-}= {
-    Mercury:"#419EBB",
-    Venus:"#EDA249",
-    Earth:"#6D2ED5",
-    Mars:"#D14C32",
-    Jupiter:"#D83A34",
-    Saturn:"#CD5120",
-    Uranus:"#1EC1A2",
-    Neptune:"#2D68F0",
-}
+
 export function Button  ({children, planet}:IButtonProps){
 return <div className={`bg-transparent hover:bg-[#D8D8D8] active:bg-[${activeColors[planet]}] text-left pl-[74px] py-3`}>
     <Heading type="h3">{children}</Heading>
